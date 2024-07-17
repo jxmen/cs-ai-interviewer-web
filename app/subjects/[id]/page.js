ff6bae1c680d120aa84741d8ef2870103e04f5c7
jxmen <sprnd645@gmail.com>
2024-07-17 19:41:14 +0900

    refator: 함수로 분리된 sessionId, csrfToken 함수 대신 직접 사용

--- app/subjects/[id]/page.js
+++ app/subjects/[id]/page.js
@@ -8,18 +8,15 @@ import { categoryMap } from "@/src/constant/categoryMap";
 import Alert from "@mui/material/Alert";
 import ChatsComponent from "@/src/component/ChatsComponent";
 
-function SubjectDetail({
-  subjectDetail,
-  isError
-}) {
-  if (!Object.keys(subjectDetail).length === 0 || isError) return (
+function SubjectDetail({ data, isError }) {
+  if (!Object.keys(data).length === 0 || isError) return (
     <Alert severity="error">데이터를 불러오는 중에 에러가 발생했습니다.</Alert>
   )
 
   return (
     <Typography variant="h4" component="h1" sx={{ padding: '10px' }}>
-      {subjectDetail.title}
-      <Chip label={categoryMap[subjectDetail.category.toLowerCase()]}
+      {data.title}
+      <Chip label={categoryMap[data.category.toLowerCase()]}
         color="primary"
         variant="outlined"
         sx={{ margin: '5px' }}
@@ -28,37 +25,19 @@ function SubjectDetail({
   );
 }
 
-export default async function SubjectDetailPage({
-  params: { id: subjectId }
-}) {
-  const sessionId = () => {
-    if (cookies().has('SESSION')) {
-      return cookies().get('SESSION').value
-    } else {
-      return null
-    }
-  }
-
-  const csrfToken = () => {
-    if (cookies().has('XSRF-TOKEN')) {
-      return cookies().get('XSRF-TOKEN').value
-    } else {
-      return null
-    }
-  }
-
-  const { data: subjectDetail, isError: isSubjectDetailFetchError } = await fetchSubjectDetail(subjectId)
+export default async function SubjectDetailPage({ params: { id: subjectId } }) {
+  const { data, isError } = await fetchSubjectDetail(subjectId)
 
   return (
     <Container maxWidth="sm" sx={{ padding: '50px' }}>
       <BackIconButton/>
-      <SubjectDetail subjectDetail={subjectDetail} isError={isSubjectDetailFetchError}/>
+      <SubjectDetail data={data} isError={isError}/>
       <Divider/>
       <ChatsComponent
         subjectId={subjectId}
         subjectDetailQuestion={subjectDetail.question}
-        sessionId={sessionId()}
-        csrfToken={csrfToken()}
+        sessionId={cookies().get('SESSION')?.value}
+        csrfToken={cookies().get('XSRF-TOKEN')?.value}
       />
 
     </Container>
