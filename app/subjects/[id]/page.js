4ed14493c112a5491d898dc7565c9767fbb34a51
jxmen <sprnd645@gmail.com>
2024-08-16 02:11:14 +0900

    refactor: 불필요한 answer v2 호출 로직 제거

--- app/subjects/[id]/page.js
+++ app/subjects/[id]/page.js
@@ -20,7 +20,6 @@ export default async function SubjectDetailPage({ params: { id: subjectId } }) {
       <ChatsComponent
         subjectId={subjectId}
         subjectDetailQuestion={data.question}
-        sessionId={cookies().get('SESSION')?.value}
         token={cookies().get('next-auth.access-token')?.value}
       />
     </Container>
